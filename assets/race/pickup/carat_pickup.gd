1dbb08d9ee92aa5f5b39796cc4da9499671e3695
KevinVG207 <kevinvg207@gmail.com>
2025-05-03 16:21:36 +0200

    Minor changes

--- assets/race/pickup/carat_pickup.gd
+++ assets/race/pickup/carat_pickup.gd
@@ -2,10 +2,10 @@ extends Node3D
 
 var mesh: MeshInstance3D
 var body_mat: ShaderMaterial
+var rot_offset := randf()*2*PI
 
 func _ready() -> void:
 	mesh = get_child(0).get_child(0) as MeshInstance3D
-	mesh.set_instance_shader_parameter("rot_offset", randf()*2*PI)
 	mesh.set_instance_shader_parameter("leaves_random_offset", Vector2(randf(), randf()))
 	body_mat = mesh.get_surface_override_material(0) as ShaderMaterial
 	
@@ -14,4 +14,5 @@ func _process(_delta: float) -> void:
 	var viewport_coords := get_viewport().get_camera_3d().unproject_position(global_position)
 	var coords = viewport_coords / Vector2(get_viewport().size) * get_window().content_scale_factor
 	mesh.set_instance_shader_parameter("offset_in_viewport", coords)
+	mesh.set_instance_shader_parameter("rot_offset", rot_offset)
 	#body_mat.set_shader_parameter("ui_scale", get_window().content_scale_factor)
